2a426ef66be94a63c430ccb8734adc324d9e16f0
Ren Estep <restep@rens-air.lan>
2020-07-18 21:06:45 -0400

    props

--- src/components/Vis/Bubble.js
+++ src/components/Vis/Bubble.js
@@ -1,34 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 import * as d3 from 'd3';
-import axios from 'axios';
 
-export default () => {
-    const [pokeTypes, setTypes] = useState([]);
-    const [selected, setSelected] = useState('');
-
-    const getData = async () => {
-        const response = await axios.get('https://pokeapi.co/api/v2/type');
-        if (response.status === 200) {
-            const pokeTypes = [];
-            const { data } = response;
-            for (let i = 0; i < data.count; i++) {
-                const resp = await axios.get(data.results[i].url);
-                if (resp.status === 200) {
-                    if (resp.data.pokemon.length > 0) {
-                        pokeTypes.push({
-                            type: data.results[i].name,
-                            count: resp.data.pokemon.length,
-                            value: resp.data.pokemon.length,
-                            data: resp.data,
-                        });
-                    }
-                }
-            }
-            setTypes(pokeTypes);
-        }
-    };
-    getData();
-
+export default ({ bubbleData, setSelected }) => {
     const width = 700;
     const height = 700;
 
@@ -37,7 +10,7 @@ export default () => {
             .pack()
             .size([width - 2, height - 2])
             .padding(3)(d3.hierarchy({ children: data }).sum((d) => d.value));
-    const stuft = pack(pokeTypes);
+    const stuft = pack(bubbleData);
 
     return (
         <svg className="BubbleChart" width={width} height={height} viewBox={`0 0 ${width} ${height}`} overflow="auto">
