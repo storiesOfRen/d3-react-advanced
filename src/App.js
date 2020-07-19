324245f56aa3a26a65bc7bd8497edcd49a107aa9
Ren Estep <restep@rens-air.lan>
2020-07-18 21:00:32 -0400

    move over data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import BubbleChart from './components/Vis/Bubble';
 import Header from './components/Header';
 import './App.scss';
 
-function App() {
+const App = () => {
+    const [pokeTypes, setTypes] = useState([]);
+
+    const getData = async () => {
+        const response = await axios.get('https://pokeapi.co/api/v2/type');
+        if (response.status === 200) {
+            const pokeTypes = [];
+            const { data } = response;
+            for (let i = 0; i < data.count; i++) {
+                const resp = await axios.get(data.results[i].url);
+                if (resp.status === 200) {
+                    if (resp.data.pokemon.length > 0) {
+                        // taking in the data that I need initially for our bubble chart constraction
+                        pokeTypes.push({
+                            type: data.results[i].name,
+                            count: resp.data.pokemon.length,
+                            value: resp.data.pokemon.length,
+                            data: resp.data,
+                        });
+                    }
+                }
+            }
+            setTypes(pokeTypes);
+        }
+    };
+    getData();
     return (
         <div className="App">
             <Header />
@@ -14,6 +39,6 @@ function App() {
             </main>
         </div>
     );
-}
+};
 
 export default App;
