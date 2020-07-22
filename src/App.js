c6ea20250af1df1a16aa87af37386829af890692
Ren Estep <restep@rens-air.lan>
2020-07-22 17:18:16 -0400

    update to get bubble data

--- src/App.js
+++ src/App.js
@@ -9,25 +9,23 @@ const App = () => {
     const [selectedType, setSelected] = useState('');
 
     const getBubbleData = async () => {
-        const response = await axios.get('https://pokeapi.co/api/v2/type');
-        if (response.status === 200) {
-            const pokeTypes = [];
-            const { data } = response;
+        try {
+            const { data } = await axios.get('https://pokeapi.co/api/v2/type');
+            const types = [];
             for (let i = 0; i < data.count; i++) {
-                const resp = await axios.get(data.results[i].url);
-                if (resp.status === 200) {
-                    if (resp.data.pokemon.length > 0) {
-                        // taking in the data that I need initially for our bubble chart constraction
-                        pokeTypes.push({
-                            type: data.results[i].name,
-                            count: resp.data.pokemon.length,
-                            value: resp.data.pokemon.length,
-                            data: resp.data,
-                        });
-                    }
+                const response = await axios.get(data.results[i].url);
+                if (response.status === 200 && response.data.pokemon.length > 0) {
+                    types.push({
+                        type: data.results[i].name,
+                        count: response.data.pokemon.length,
+                        value: response.data.pokemon.length,
+                        data: response.data,
+                    });
                 }
             }
-            setTypes(pokeTypes);
+            setTypes(types);
+        } catch (err) {
+            console.error(err);
         }
     };
     getBubbleData();
